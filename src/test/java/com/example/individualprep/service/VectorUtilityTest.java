fe7965bfd6e0887b389b29a27c29af85853c3525
kimnzh <hakimnizami15@gmail.com>
2026-02-12 09:35:03 +0700

    chore: added test files

---
+++ src/test/java/com/example/individualprep/service/VectorUtilityTest.java
@@ -0,0 +1,13 @@
+package com.example.individualprep.service;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class VectorUtilityTest {
+
+    @Test
+    void example() {
+        // TODO: Implement unit tests for VectorUtility methods
+        assertEquals(0.0, 0.0, 1e-9);
+    }
+}
