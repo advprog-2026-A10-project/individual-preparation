19014ffdef00a1cc7595f395bfbe2b4518aa38cb
firstaqila <saffana.firsta@gmail.com>
2026-02-12 21:25:45 +0700

    Added unit test for vector dot product

--- src/test/java/com/example/individualprep/service/VectorUtilityTest.java
+++ src/test/java/com/example/individualprep/service/VectorUtilityTest.java
@@ -5,9 +5,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class VectorUtilityTest {
 
+    private final VectorUtility vectorUtility = new VectorUtility();
+
     @Test
-    void example() {
-        // TODO: Implement unit tests for VectorUtility methods
-        assertEquals(0.0, 0.0, 1e-9);
+    void testDotProductSuccess() {
+        double[] v1 = {1.0, 3.0, -5.0};
+        double[] v2 = {4.0, -2.0, -1.0};
+        assertEquals(3.0, vectorUtility.dotProduct(v1, v2));
+    }
+
+    @Test
+    void testDotProductLengthMismatch() {
+        double[] v1 = {1.0, 2.0};
+        double[] v2 = {1.0, 2.0, 3.0};
+        assertThrows(IllegalArgumentException.class, () -> vectorUtility.dotProduct(v1, v2));
     }
 }
